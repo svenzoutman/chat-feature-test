8e2b6285685aac4b2c1e04870f8400705c9a80f9
Sven Zoutman <sven.zoutman@gmail.com>
2023-05-10 20:08:29 +0200

    olm

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const io = socketio(server)
 //bodyparser middleware
 app.use(express.json());
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const methodOverride = require("method-override");
 const {
   v4: uuidv4
